8754c2a94acc1f4363d09fdb39147a2eb8cba616
Corbin Enterline <1692@holbertonschool.com>
2020-02-06 20:50:47 +0000

    Fixed whitespace after removing abs

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,7 +6,7 @@
  * main - Going to call the functions below.
  * Description: This function assigns a random number to n.
  * It then checks the last digit of n to see if it is above 6 or higher,
- * 0, or 5 to 1. It then prints this result. 
+ * 0, or 5 to 1. It then prints this result.
  * d stands for Last Digit
  * n % 10 uses a modulo operator to divide n by ten, finding it's last digit.
  * Return: The last digit, and it's associated group.
