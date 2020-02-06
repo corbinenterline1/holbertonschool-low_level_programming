654bdc5d221d12cb61fa3d8c2068a6d7ec3dc323
Corbin Enterline <1692@holbertonschool.com>
2020-02-06 06:06:33 +0000

    Fixed comments

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 /**
  * main - Entry point
- * @char displays char byte value
- * @int displays int byte value
- * @long int displays double byte value
- * @long long int displays double byte value
- * @float displays float byte value
+ * char displays char byte value
+ * int displays int byte value
+ * long int displays double byte value
+ * long long int displays double byte value
+ * float displays float byte value
+ * Return: The full list of data types with their byte values!
  */
 int	main(void)
 {
