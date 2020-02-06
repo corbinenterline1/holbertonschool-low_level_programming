c64ff9b484e1a2b243cb2e2527c8b4c9b8e4a74b
Corbin Enterline <1692@holbertonschool.com>
2020-02-06 02:21:32 +0000

    Let's try again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
-int main()
+/**
+ * 4-puts - Outputting a line to the terminal
+(* a blank line
+ * Description: This command puts forward a certain text string, then exits.
+ * Return: "Programming is like building a multilingual puzzle
+*/
+int	main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle ");
-  return(0);
+	puts("Programming is like building a multilingual puzzle ");
+	return (0);
 }
