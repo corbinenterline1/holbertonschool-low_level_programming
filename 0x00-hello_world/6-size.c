fa73048ac111958aab0aa8202e74cb505e38d744
Corbin Enterline <1692@holbertonschool.com>
2020-02-06 04:25:57 +0000

    First pass 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * puts - Enters the string
+ * puts - x4
+ * Return: The Beautiful Bytes
+ */
+int	main(void)
+{
+	puts("Size of a char: 1 byte(s)");
+	puts("Size of an int: 4 byte(s)");
+	puts("Size of a long int: 4 byte(s)");
+	puts("Size of a long long int: 8 byte(s)");
+	puts("Size of a float: 4 byte(s)");
+	return (0);
+}
