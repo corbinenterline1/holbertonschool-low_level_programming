0e212fac8964bcb1763ce27391c4235a6b0c7158
Corbin Enterline <1692@holbertonschool.com>
2020-02-06 05:04:07 +0000

    Tried printf approach

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,19 +2,20 @@
 /**
  * main - Entry point
  * puts - Enters the string
- * puts -
- * puts - 
- * puts -
- * puts -
- * puts -
+ * puts - x4
  * Return: The Beautiful Bytes
  */
 int	main(void)
 {
-	puts("Size of a char: 1 byte(s)");
-	puts("Size of an int: 4 byte(s)");
-	puts("Size of a long int: 4 byte(s)");
-	puts("Size of a long long int: 8 byte(s)");
-	puts("Size of a float: 4 byte(s)");
-	return (0);
+	char	charType;
+	int	intType;
+	double	doubleType;
+	float	floatType;
+
+	printf("Size of a char: %ld byte(s)\n", sizeof(charType));
+	printf("Size of a int: %ld byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(intType));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(doubleType));
+	printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
+	return(0);
 }
