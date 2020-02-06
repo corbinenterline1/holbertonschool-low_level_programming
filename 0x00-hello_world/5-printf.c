5f897fdc1f0abfe8a2e8686d6891eed7b9b40f54
Corbin Enterline <1692@holbertonschool.com>
2020-02-06 03:01:42 +0000

    First pass 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * puts - Enters the string
+ * Return: The string!
+ */
+int	main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
