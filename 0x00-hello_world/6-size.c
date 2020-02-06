b729892faa337027a66cf33427956247f821996f
Corbin Enterline <1692@holbertonschool.com>
2020-02-06 16:39:40 +0000

    Figured out problem immediately upon waking

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,18 +5,19 @@
  * int displays int byte value
  * long int displays double byte value
  * long long int displays double byte value
- * float displays float byte value
+ * float displays int byte value
  * Return: The full list of data types with their byte values!
  */
 int	main(void)
 {
 	char	c;
 	int	i;
+	long	l;
 	double	d;
 
 	printf("Size of a char: %ld byte(s)\n",	sizeof(c));
 	printf("Size of an int: %ld byte(s)\n",	sizeof(i));
-	printf("Size of a long int: %ld byte(s)\n",	sizeof(d));
+	printf("Size of a long int: %ld byte(s)\n",	sizeof(l));
 	printf("Size of a long long int: %ld byte(s)\n",	sizeof(d));
 	printf("Size of a float: %ld byte(s)\n",	sizeof(i));
 	return (0);
