77340a31eb37d0bacd061a1e739335d433ff9150
Corbin Enterline <1692@holbertonschool.com>
2020-02-07 05:55:12 +0000

    First pass fail

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * This command is printing 0-9
+ * a = int for 0-9
+ * while loops while a is less than 10
+ * 1st putchar puts the current number count down
+ * 2nd putchar puts the,
+ * 3rd putchar puts the space after ,
+ * Return: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+ */
+int main(void)
+	{
+	int p = 48;
+	int i = 48;
+
+	for (i = 48; i <= 57; i++)
+	{
+	for (p = 48; p <= 57; p++)
+	{
+	putchar(p);
+	}
+	putchar(i);
+	putchar(',');
+	putchar(' ');
+	}
+	if (i && p == 57)
+	{
+	putchar('\n');
+	}
+	return (0);
+}
+
