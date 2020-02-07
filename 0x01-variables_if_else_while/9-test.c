20f63246dcd7ce8208389bbaf55b12cc11991339
Corbin Enterline <1692@holbertonschool.com>
2020-02-07 03:35:48 +0000

    Example file

---
+++ 0x01-variables_if_else_while/9-test.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * This command is printing 0-9
+ * a = int for 0-9
+ * while loops while a is less than 10
+ * 1st putchar puts the current number count down
+ * 2nd putchar puts the ,
+ * 3rd putchar puts the space after ,
+ * Return: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+ */
+int main(void)
+	{
+	int i = '0';
+
+	for (i = '0'; i <= '9';)
+	if (i != '9')
+	{	
+	putchar(i);
+	putchar(',');
+	putchar(' ');
+	i++;
+	}
+	else
+	{
+	putchar('\n');
+	}
+	return (0);
+}
