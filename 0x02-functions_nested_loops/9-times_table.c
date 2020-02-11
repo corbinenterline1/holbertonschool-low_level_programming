8e193bcd3978c8fd900abf9476f8f1fb81f938cb
Corbin Enterline <1692@holbertonschool.com>
2020-02-11 23:37:05 +0000

    fixed comments

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,9 +2,14 @@
 
 /**
  * times_table - Prints a times table from 0-9
- * First for loop sets e as number of total lines
- * Then sets c=0, then nested for to set the multiplier, d
- * d starts at 0, is multiplied by c, then adds ', ' and advances
+ * int e is number of lines
+ * int d is the digit
+ * int r is the return of the multiplication
+ * first for loop sets the number of lines, then prints a 0
+ * 2nd for loop sets the digit for the rest of the line
+ * we then multiply d * e to get r
+ * if r is less than 10, put ',  'r
+ * if greater than, put ', 'rr
  * Return: prints all the times of the day, minute by minute
  */
 void times_table(void)
