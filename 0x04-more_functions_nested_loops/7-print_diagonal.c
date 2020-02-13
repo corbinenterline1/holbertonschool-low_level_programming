040cebe07da211129445e4e0e48ce504d8a35f4a
Corbin Enterline <corbinenterline1@gmail.com>
2020-02-13 20:41:52 +0000

    First pass on 7, updated header

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - c
+ * @n: is the diagonal line count from main function
+ * First for loop is rows
+ * if n, or a consequently, is 0 or less, newline
+ * otherwise, for loop setting number of spaces
+ * finish first for loop with the diagonal line & newline
+ * Result: diagonal lines, dictated in length by n
+ */
+void print_diagonal(int n)
+	{
+	int a;
+	int b;
+
+	for (a = 1; a <= n; a++)
+		{
+		if (a <= 0)
+			{
+			_putchar('\n');
+			}
+		else
+			{
+			for (b = 1; b < a; b++)
+				{
+				_putchar(' ');
+				}
+			_putchar(92);
+			_putchar('\n');
+			}
+		}
+	}
