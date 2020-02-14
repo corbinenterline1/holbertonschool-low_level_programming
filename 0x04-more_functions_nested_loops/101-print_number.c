6940a05995e856dce22a6b909f1ff31e25d394ef
Corbin Enterline <corbinenterline1@gmail.com>
2020-02-14 20:43:08 +0000

    Unfinished, work from home

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - prints 0-9, minus 2 & 4
+ * a@: integer used in while loop for printing
+ * Result: 01356789
+ */
+void print_number(int n)
+	{
+	int a;
+	unsigned int b;
+#include "holberton.h"
+
+/**
+ * print_most_numbers - prints 0-9, minus 2 & 4
+ * a@: integer used in while loop for printing
+ * Result: 01356789
+ */
+void print_number(int n)
+	{
+	int a;
+	unsigned int b;
+
