d2a5baf02bbb79120215c23c2326cfcca879e620
Corbin Enterline <corbinenterline1@gmail.com>
2020-02-19 00:13:53 +0000

    Made betty happy

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,14 +17,17 @@ int _strlen(char *s)
 	return (l);
 }
 /**
- * puts2 - Prints every other line of string
- * @str:  Input string
- * i is counter integer
- * l is length of string
- * while counter is less than length minus 1 (account for line break)
- * putchar of string at i, then advance i twice
- * After loop, finish with newline
- * Result: The string kinda printed!
+ * puts_half - prints second half of string
+ * @str: input string
+ * int i is counter
+ * i equals string length
+ * if i modulo 2 has results
+ * increment i, then try again
+ * once i % 2 has no remainder, divide i by 2
+ * this gets half the string value (accounts for odd numbered string)
+ * while string at i position doesn't equal line break
+ * putchar of string at spot i, then advance i, then loop
+ * Result: The string printed half!
  */
 void puts_half(char *str)
 {
