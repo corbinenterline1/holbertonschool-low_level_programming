4661d4f46c519052719cdd41bed6291d06157a1f
Corbin Enterline <corbinenterline1@gmail.com>
2020-02-20 17:29:43 +0000

    Fixed comments on 1, and backup of 1 (while instead of for)

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,8 +1,15 @@
 #include "holberton.h"
 
 /**
+ * _strncat - Concanates string src to end of dest by n number of bytes
  * @dest: destination string
  * @src: source string that is put at end of dest string
+ * @n: number of bytes to copy
+ * first for loop sets int a to end of destination string
+ * second for loop sets b to 0, then adds source to destination
+ * at spot where a + b (this means a is end of dest, while b starts at 0
+ * and increments). Go until either n is 0, or source is at null byte.
+ * Return: destination with appended string of n bytes
  */
 char *_strncat(char *dest, char *src, int n)
 {
--- 0x06-pointers_arrays_strings/1backup.c
+++ 0x06-pointers_arrays_strings/1backup.c
@@ -18,8 +18,13 @@ int _strlen(char *s)
 	return (l);
 }
 /**
+ * _strncat - copies string src onto end of dest by n bytes
  * @dest: destination string
  * @src: source string that is put at end of dest string
+ * set a char pointer (ptr) to end of dest with strlen
+ * while source isn't null byte, and n isn't zero
+ * put src byte where ptr is, then advance both, and decrease n
+ * this ensures you don't put too many bytes
  */
 char *_strncat(char *dest, char *src, int n)
 {
