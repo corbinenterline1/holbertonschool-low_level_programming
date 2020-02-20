4804daca299796d84de0565c59de77618e581704
Corbin Enterline <corbinenterline1@gmail.com>
2020-02-20 17:30:34 +0000

    Made betty happy for 1'

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,7 +16,8 @@ char *_strncat(char *dest, char *src, int n)
 	int a;
 	int b;
 
-	for (a = 0; dest[a] != '\0'; a++);
+	for (a = 0; dest[a] != '\0'; a++)
+	;
 	for (b = 0; src[b] != '\0' && n != 0; b++, n--)
 	{
 		dest[a + b] = src[b];
