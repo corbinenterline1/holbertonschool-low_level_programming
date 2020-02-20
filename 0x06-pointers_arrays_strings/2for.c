0944dbdd61eb75735f80e2d055b39192a4c96e1b
Corbin Enterline <corbinenterline1@gmail.com>
2020-02-20 20:38:31 +0000

    Redid 2, backed up other methods for pld

--- 0x06-pointers_arrays_strings/2for.c
+++ 0x06-pointers_arrays_strings/2for.c
@@ -16,7 +16,7 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (a = 0; src[a] != '\0' && a < n; a++)
 		dest[a] = src[a];
-	for ( ; a < n; a++)
+	for (; a < n; a++)
 	dest[a] = '\0';
 	return (dest);
 }
