da420cf0d5f7169716d7476259cda12cfd58aecf
Corbin Enterline <corbinenterline1@gmail.com>
2020-02-25 03:31:08 +0000

    First pass on trick question #9, updated header

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * set_string - Sets the value of a pointer to a char.
+ * @s: double pointer
+ * @to: char to set value to
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
