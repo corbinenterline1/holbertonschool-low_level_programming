113f2c7b85d0143dcdfc558cf151f3a48f915dab
Corbin Enterline <1692@holbertonschool.com>
2020-03-04 02:08:37 +0000

    First push 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * **alloc_grid - allocates a 2dimensional array in memory
+ * @width: width of the array
+ * @height: height of the array
+ * set pointer to pointers an allocated memory of size of height
+ * then for each row, allocate memory by size of width
+ * then initialize all elements with 0
+ * Return: the grid if successfull, NULL otherwise
+ */
+int **alloc_grid(int width, int height)
+{
+	int **g;
+	int a;
+	int b;
+	int c;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	g = malloc(height * sizeof(int *));
+	if (g == NULL)
+		return (NULL);
+	for (a = 0; a < height; a++)
+	{
+		g[a] = malloc(width * sizeof(int));
+		if (g[a] == NULL)
+			return (NULL);
+	}
+	for (b = 0; b < height; b++)
+	{
+		for (c = 0; c < width; c++)
+			g[b][c] = 0;
+	}
+	return (g);
+}
