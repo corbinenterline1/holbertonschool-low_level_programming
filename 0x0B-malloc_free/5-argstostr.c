ad63f2e32953eeaa9e1b81e7c5e688216261aed2
Corbin Enterline <1692@holbertonschool.com>
2020-03-04 05:29:28 +0000

    Removed nested for loops into own function for conc on 5, updated header

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,70 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - gets length of string
+ * @s: input string
+ * Increments string & l(ength) while s doesn't equal 0(line break)
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s != 0)
+	{
+	s++;
+	l++;
+	}
+	return (l);
+}
+/**
+ * *strcatcust - custom str concatenation
+ * @s1: string to be copied onto
+ * @s2: string to copy
+ * first for loop gets a to end of s1
+ * 2nd for loop copies s2 over to s1
+ * then add terminator
+ * Return: Concatenated s1
+ */
+char *strcatcust(char *s1, char *s2)
+{
+	int a, b;
+
+	for (a = 0; s1[a] != '\0'; a++)
+	;
+	for (b = 0; s2[b] != '\0'; b++, a++)
+		s1[a] = s2[b];
+	s1[a] = '\0';
+	return (s1);
+}
+/**
+ * *argstostr - Creates a string from the arguments to main
+ * @ac: number of arguments
+ * @av: input pointer to strings
+ * first for loop gets total length of all strings, +1 for newlines
+ * then allocate memory array of l size to pointer dum
+ * second for loop runs the custom string concentenator(sp?), adds newline
+ * Return: NULL if anything's goofy, otherwise dum
+ */
+char *argstostr(int ac, char **av)
+{
+	int a, b, l;
+	char *dum;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (a = 0; a < ac; a++)
+		l += (_strlen(av[a]) + 1);
+	dum = malloc(l * sizeof(*dum));
+	if (dum == NULL)
+		return (NULL);
+	for (b = 0; b < ac; b++)
+	{
+		strcatcust(dum, av[b]);
+		strcatcust(dum, "\n");
+	}
+	if (dum == NULL || dum == 0)
+		return (NULL);
+	return (dum);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -149,4 +149,8 @@ int **alloc_grid(int width, int height);
 
 void free_grid(int **grid, int height);
 
+char *argstostr(int ac, char **av);
+
+char *strcatcust(char *s1, char *s2);
+
 #endif
