6d9f393863c844315b6c170c69e85428422f9a27
Corbin Enterline <1692@holbertonschool.com>
2020-03-06 00:45:27 +0000

    Rewrote 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,30 +3,6 @@
 #include <stdlib.h>
 
 /**
- * *_memset - Fills first 'n' bytes of memory area pointed to by 's' with 'b'.
- * @s: pointer to memory area to be written to
- * @b: constant byte to fill memory pointed to by s
- * @n: number of bytes to write b into s (from first n bytes of memory)
- * Set a dummy pointer to s's address
- * while n is greater than 0
- * cast b to where dummy pointer is
- * increment dummy pointer, decrement n
- * Return: s pointer to newly written over memory area
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	char *d = s;
-
-	while (n > 0)
-	{
-	*d = b;
-	d++;
-	n--;
-	}
-	return (s);
-}
-
-/**
  * *_calloc - Allocates memory for an array, with memory set to 0
  * @nmemb: number of elements in array
  * @size: size of each element of the array
@@ -42,11 +18,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	a = nmemb * size;
-	p = malloc(a * sizeof(unsigned int));
+	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
-	if (p != 0)
-		_memset(p, 0, a);
+	for (a = 0; a < (nmemb * size); a++)
+		p[a] = 0;	_
 	return (p);
 }
