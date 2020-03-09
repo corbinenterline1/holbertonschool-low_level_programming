51985f78971ab8325b3f585f62cfb9c6d427d1b3
Corbin Enterline <1692@holbertonschool.com>
2020-03-09 21:12:59 +0000

    First push 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints file name
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
