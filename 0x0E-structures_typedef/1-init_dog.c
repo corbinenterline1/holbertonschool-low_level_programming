93c634a4dcc5b749ca2698541940a788ba9515e8
Corbin Enterline <1692@holbertonschool.com>
2020-03-09 23:50:18 +0000

    unsure why failing 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * init_dog - initializes a variable of type struct dog
  * @d: input structure
@@ -11,7 +12,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-	(*d).name = name;
+	d->name = name;
 	d->age = age;
 	d->owner = owner;
 	}
