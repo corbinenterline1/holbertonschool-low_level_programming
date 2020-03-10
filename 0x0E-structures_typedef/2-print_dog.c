6306b6b7e08114463fe046b784b5b1066491c1ba
Corbin Enterline <1692@holbertonschool.com>
2020-03-10 00:15:34 +0000

    first push 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_dog - prints a struct dog in a readable format
+ * @d: input dog struct
+ * check if struct is NULL, if so return (no number, cause print_dog is void)
+ * then start checking if elements are null
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		printf("(nil)");
+	else
+		printf("Name: %s\n", d->name);
+	if (d->age != d->age)
+		printf("(nil)");
+	else
+		printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
+		printf("(nil)");
+	else
+		printf("Owner: %s\n", d->owner);
+}
