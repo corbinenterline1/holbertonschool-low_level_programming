dfb93d70e48de326c5ccc0d3aef4d5d5486a8e9a
Corbin Enterline <1692@holbertonschool.com>
2020-03-10 00:17:03 +0000

    Fixed null prints to proper format as directed

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,15 +13,15 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		printf("(nil)");
+		printf("Name: (nil)");
 	else
 		printf("Name: %s\n", d->name);
 	if (d->age != d->age)
-		printf("(nil)");
+		printf("Age: (nil)");
 	else
 		printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
-		printf("(nil)");
+		printf("Owner: (nil)");
 	else
 		printf("Owner: %s\n", d->owner);
 }
