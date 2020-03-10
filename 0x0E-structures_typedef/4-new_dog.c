dec5bb0af5eb31d8e26d8ec772ffcf04256b14f4
Corbin Enterline <1692@holbertonschool.com>
2020-03-10 04:19:42 +0000

    Testing old one with fix for rewrite 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,73 +3,91 @@
 #include <stdlib.h>
 
 /**
- * *_custstrdup - refined version of strdup (my old one sucked)
- * @str: input pointer to string
- * get length of string with first for
- * malloc string + 1 (for terminator)
- * all throughout, check for NULL at appropriate time
- * second for loop copies string to new cpy (copy)
- * Return: cpy
+ * *_memcpy - Copies 'n' bytes from memory area 'src' to memory area 'dest'
+ * @dest: destination memory area
+ * @src: source memory area
+ * @n: number of bytes to copy from src to dest
+ * Set a pointer 'd'  to dest memory area
+ * Set a pointer 's' to src memory area
+ * While n is greater than 0;
+ * set s into d, increment both, decrement n
+ * Return: pointer to dest
  */
-char *_custstrdup(char *str)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int a, b;
-	char *cpy;
+	char *d = dest;
+	char *s = src;
 
-	a = b = 0;
-	if (str == 0)
-		return (NULL);
-	for (; str[a] != '\0'; a++, b++)
-	;
-	cpy = malloc(sizeof(char) * (b + 1));
-	if (cpy == NULL)
+	while (n > 0)
 	{
-		free(cpy);
-		return (NULL);
+		*d++ = *s++;
+		n--;
 	}
-	for (a = 0; str[a] != '\0'; a++)
-		cpy[a] = str[a];
-	cpy[a] = '\0';
-	return (cpy);
+	return (dest);
 }
-
-
 /**
- * *new_dog - creates a new dog struct
+ * *_strdup - returns a pointer to a new string that's duplicated from str
+ * @str: input string
+ * if str isn't null, get length of string with l
+ * Then allocate array of cpy to `l` elements
+ * if the copy is empty, or null, return null, else run memcpy
+ * Return: cpy if successfull, NULL otherwise
+ */
+char *_strdup(char *str)
+{
+	char *cpy;
+	int l;
+
+	if (str == NULL)
+		return (NULL);
+	{
+		for (l = 0; str[l] != '\0'; l++)
+		;
+		l++;
+		cpy = malloc(l * sizeof(char));
+		if (cpy == NULL || cpy == 0)
+			return (NULL);
+		else if (cpy)
+			_memcpy(cpy, str, l);
+		return (cpy);
+	}
+}
+/**
+ * new_dog - creates a new struct dog
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
- * check if any input strings are NULL
- * get length of name (ln) and owner (lo)
- * malloc the new struct dog new_dog, check if malloc failed
- * malloc the copy of name (cpyn), check if malloc failed
- * malloc the copy of owner (cpyo), check if malloc failed
- * copy name to cpyn, add terminator. Do same for owner
- * Then, set elements of struct dog new_dog
- * Return: new_dog struct dog
+ * allocate memory for new struct dog
+ * typical memory checks throughout after malloc
+ * strdup name onto cpyn (copy name)
+ * stdup owner onto cpyo (owner name)
+ * then set struct elements
+ * Return: new dog struct
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
+	char *cpyn, *cpyo;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
-	new_dog->name = _custstrdup(name);
-	if (new_dog->name == NULL)
+	cpyn = _strdup(name);
+	if (cpyn == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
+	cpyo = _strdup(owner);
+	if (cpyo == NULL)
+	{
+		free(cpyn);
+		free(new_dog);
+		return (NULL);
+	new_dog->name = cpyn;
 	new_dog->age = age;
-	new_dog->owner = _custstrdup(owner);
-	if (new_dog->owner == NULL)
-	{
-		free(new_dog->name);
-		free(new_dog);
-		return (NULL);
-	}
+	new_dog->owner = cpyo;
 	return (new_dog);
 }
