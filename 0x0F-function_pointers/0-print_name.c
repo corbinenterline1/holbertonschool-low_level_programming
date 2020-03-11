b344201ead8cdf7473ee0b1bf49d4036298da477
Corbin Enterline <1692@holbertonschool.com>
2020-03-11 18:26:59 +0000

    First push 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - function that prints name using another function
+ * @name: input name
+ * @f: function to be called
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
