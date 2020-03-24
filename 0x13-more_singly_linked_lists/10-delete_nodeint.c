ce6a12d5328ac0e96f9e363c77df87a2993cb698
Corbin Enterline <1692@holbertonschool.com>
2020-03-24 23:16:17 +0000

    Rewrote 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -26,7 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	while (cpy)
 	{
-		if (c == index - 1)
+		if (c == index)
 		{
 			tmp = cpy->next;
 			cpy->next = tmp->next;
