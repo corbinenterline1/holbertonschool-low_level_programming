989c0eb42a5af44071bab9b443dca120d9ad98b6
Corbin Enterline <1692@holbertonschool.com>
2020-03-24 21:03:20 +0000

    My version

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,42 +1,42 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts new node at index n
- * @head: pointer to pointer of head node of linked list
- * @idx: index of the list where the new node should be added
+ * insert_nodeint_at_index - Inserts a new node at a given position
+ * @head: pointer to pointer to head node of linked list
+ * @idx: index of the list where the new node should be added.
  * @n: data for new node
- * Return: pointer to new node
+ * Return: address of the new node, or NULL if it failed
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i;
-	listint_t *new;
-	listint_t *p;
+	listint_t *trav, *tmp;
+	unsigned int c;
 
-	if (head == NULL)
+	if (*head == NULL || head == NULL)
 		return (NULL);
-	new = malloc(sizeof(listint_t));
-	p = *head;
-
-	if (new == NULL)
+	trav = *head;
+	c = 0;
+	while (trav && c != idx)
+	{
+		trav = trav->next;
+		if (trav == NULL)
+			return (NULL);
+		c++;
+	}
+	tmp = malloc(sizeof(listint_t));
+	if (tmp == NULL)
 		return (NULL);
-	new->n = n;
-	for (i = 0; p != NULL && i < idx - 1; i++)
-		p = p->next;
-	if (p == NULL && i != 0)
+	tmp->n = n;
+	if (idx == 0)
 	{
-		free(new);
-		return (NULL);
+		tmp->next = *head;
+		*head = tmp;
+		return (tmp);
 	}
-	if (i == 0)
-	{
-		*head = new;
-		new->next = p;
-	}
-	else
-	{
-		new->next = p->next;
-		p->next = new;
-	}
-	return (new);
+	tmp->next = trav->next;
+	trav->next = tmp;
+	return (tmp);
 }
