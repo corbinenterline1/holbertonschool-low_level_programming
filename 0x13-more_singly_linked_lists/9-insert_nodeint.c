7e85141a4fa3715d5702888fa90fac5d1b160a63
Corbin Enterline <1692@holbertonschool.com>
2020-03-24 19:58:05 +0000

    Testing theory for sean

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts new node at index n
+ * @head: pointer to pointer of head node of linked list
+ * @idx: index of the list where the new node should be added
+ * @n: data for new node
+ * Return: pointer to new node
+ */
+listint_t *insert_nodeint_at_index(instint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	listint_t *new;
+	listint_t *p;
+
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(listint_t));
+	p = *head;
+
+	if (new == NULL)
+		return (NULL)
+	new->n = n;
+	for (i = 0; p != NULL && i + 1 < idx; i++)
+		p = p->next;
+	if (i == 0)
+	{
+		*head = new;
+		new->next = p;
+	}
+	else
+	{
+		new->next = p->next;
+		p->next = new;
+	}
+	return (new);
+}
+
+
+
+}
