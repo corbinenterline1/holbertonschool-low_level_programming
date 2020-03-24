a96a4e33c09b70ccfab6035159d02211168053f4
Corbin Enterline <1692@holbertonschool.com>
2020-03-24 20:34:08 +0000

    Reverted 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,13 +6,17 @@
  */
 void free_listint2(listint_t **head)
 {
+	listint_t *tmp, *trav;
 
 	if (*head == NULL || head == NULL)
 		return;
-	while (head)
+	trav = *head;
+	while (head && trav)
 	{
-		free(*head);
-		*head = (*head)->next;
+		tmp = trav;
+		trav = trav->next;
+		free(tmp);
 	}
+	free(trav);
 	head = NULL;
 }
