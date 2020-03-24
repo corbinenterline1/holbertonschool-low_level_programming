c5082f6f594bf6b514f43e1afbfea6ca28a78a7c
Corbin Enterline <1692@holbertonschool.com>
2020-03-24 19:30:49 +0000

    Updating for requirements check

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
 
 /**
  * struct listint_s - singly linked list
@@ -11,8 +14,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 int _putchar(char c);
