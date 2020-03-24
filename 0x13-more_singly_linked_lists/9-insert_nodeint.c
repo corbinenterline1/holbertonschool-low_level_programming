b4c80f734107cada3c7af6ce37ef65a60e5a8863
Corbin Enterline <1692@holbertonschool.com>
2020-03-24 22:26:52 +0000

    Tweaking first if of 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int c = 0;
 
 	new = malloc(sizeof(listint_t));
-	if (new == NULL || head == NULL || *head == NULL)
+	if (new == NULL || head == NULL)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
