8f20adba753953ba4df7c18be4b88d583be26cbc
Corbin Enterline <1692@holbertonschool.com>
2020-03-24 23:26:29 +0000

    Fixed syntax on first null check

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *cpy, *tmp;
 	unsigned int c = 0;
 
-	if (*head == NULL)/* if list is empty */
+	if (*head == NULL || head == NULL)/* if list is empty */
 		return (-1);
 	cpy = *head;
 	if (index == 0)
