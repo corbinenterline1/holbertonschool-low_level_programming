5548c5961bf9e5ddeabb04a016460287215406ef
Corbin Enterline <1692@holbertonschool.com>
2020-03-24 22:32:52 +0000

    Reverting to mine

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,42 +1,42 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts new node at index n
- * @head: pointer to pointer of head node of linked list
- * @idx: index of the list where the new node should be added
+ * insert_nodeint_at_index - Inserts a new node at a given position
+ * @head: pointer to pointer to head node of linked list
+ * @idx: index of the list where the new node should be added.
  * @n: data for new node
- * Return: pointer to new node
+ * Return: address of the new node, or NULL if it failed
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i;
-	listint_t *new;
-	listint_t *p;
+	listint_t *curr, *new;
+	unsigned int c = 0;
 
-	if (head == NULL)
-		return (NULL);
 	new = malloc(sizeof(listint_t));
-	p = *head;
-
-	if (new == NULL)
+	if (new == NULL || head == NULL)
 		return (NULL);
 	new->n = n;
-	for (i = 0; i + 1 < idx; i++)
-		p = p->next;
-	if (p == NULL && i != 0)
-	{
-		free(new);
-		return (NULL);
-	}
-	if (i == 0)
-	{
+	new->next = NULL;
+	if (idx == 0)
+	{/* adding at head */
+		new->next = *head;
 		*head = new;
-		new->next = p;
+		return (new);
 	}
-	else
+	curr = *head;
+	while (curr)
 	{
-		new->next = p->next;
-		p->next = new;
+		if (c == idx - 1)
+		{
+			new->next = curr->next;
+			curr->next = new;
+			return (new);
+		}
+		curr = curr->next;
+		c++;
 	}
-	return (new);
+	return (NULL);
 }
