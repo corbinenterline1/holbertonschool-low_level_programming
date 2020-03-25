2432c193f1903b3fb6dc1dec73dd37dc351fdab1
Corbin Enterline <1692@holbertonschool.com>
2020-03-25 01:09:38 +0000

    Tweak by removing last if

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -42,8 +42,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			free(tmp);
 			return (1);
 		}
-		if (cpy->next != NULL)
-			cpy = cpy->next;
+		cpy = cpy->next;
 		c++;
 	}
 	return (-1);
