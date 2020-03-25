7461fae18fab57031acde61e6b586c0af147c95c
Corbin Enterline <1692@holbertonschool.com>
2020-03-25 00:24:03 +0000

    Attempting to account for index 0

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -28,7 +28,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		if (cpy == NULL)
 			return (-1);
-		if (c == index - 1)
+		if (c == index - 1 || c == index)
 		{
 			tmp = cpy->next;
 			cpy->next = tmp->next;
