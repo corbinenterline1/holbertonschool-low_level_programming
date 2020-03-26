be345293cde803912129222d824570f178dd9b4a
Corbin Enterline <1692@holbertonschool.com>
2020-03-26 22:33:31 +0000

    betty

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,9 +1,11 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
+ * print_binary - Prints the binary representation of a number
+ * @n: input number to be printed
+ * Recursive function, each iteration moves over 1 bit
+ * then they all putchar the result of bitwise AND the number & 1
+ * in ascii value
  */
 void print_binary(unsigned long int n)
 {
