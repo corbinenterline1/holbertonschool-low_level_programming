058f18bec381bb30af74f2c5e9fd6dfae2676422
Corbin Enterline <1692@holbertonschool.com>
2020-03-27 20:07:14 +0000

    First actual push

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,10 +1,15 @@
 #include "holberton.h"
-
 /**
- * get_endianness - checks the endianness
- * Return: 1 if little endian, 0 if big endian
+ * get_endianness - Checks the endianness
+ * Return: 1 for little endian, 0 if big endian
  */
 int get_endianness(void)
 {
-	return (1);
+	unsigned int i = 1;
+	char *c = (char *)&i;
+
+	if (*c)
+		return (1);
+	else
+		return (0);
 }
