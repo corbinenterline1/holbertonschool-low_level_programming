d2ea1ff9ef4240e1002f17cea4d5658692419d1d
Corbin Enterline <1692@holbertonschool.com>
2020-05-02 21:38:43 +0000

    First push 2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - Adds new node at head of doubly linked list
+ * @head: double pointer to head of linked list
+ * @n: integer data for new node
+ * Return: New node pointer
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	new->prev = NULL;
+	return (new);
+}
