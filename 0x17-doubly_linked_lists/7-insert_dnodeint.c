66690c0488dcfdd24c4322aaa1f8dd6b23b73798
Corbin Enterline <1692@holbertonschool.com>
2020-05-04 03:32:17 +0000

    Rewrite

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,44 +9,56 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *curr, *new = malloc(sizeof(dlistint_t));
-	unsigned int c = 0;
+	dlistint_t *curr, *new;
+	unsigned int c = 0, a = 0;
 
-	if (new == NULL || h == NULL)
+	curr = *h;
+	a = listy(curr);
+	if (a == idx)
 	{
-		free(new);
-		return (NULL);
+		add_dnodeint_end(&curr, n);
+		return (curr);
 	}
-	curr = *h, new->n = n;
+	new = malloc(sizeof(dlistint_t));
+	new->n = n;
 	new->next = NULL, new->prev = NULL;
-	if (idx == 0 && curr == NULL)
-	{/* will 3 on 1 line work below? */
-		new->next = NULL, new->prev = NULL, *h = new;
-		return (new);
-	}
+	if (new == NULL)
+		return (NULL);
 	while (curr)
 	{
 		if (idx == 0)
 		{
-			new->prev = NULL, new->next = curr;
-			curr->prev = new, *h = new;
+			new->next = curr, curr->prev = new, *h = new;
 			return (new);
 		}
 		if (c == idx - 1)
 		{
+			new->n = n;
 			curr->next->prev = new, new->next = curr->next;
 			new->prev = curr, curr->next = new;
 			return (new);
 		}
-		if (c < idx && curr->next->next == NULL)
-		{
-			curr = curr->next, new->prev = curr;
-			new->next = NULL, curr->next = new;
-			return (new);
-		}
+		else if (curr->next->next == NULL)
+			return (NULL);
 		curr = curr->next;
 		c++;
 	}
 	free(new);
 	return (NULL);
+}
+/**
+ * listy - non constant list length finder
+ * @h: pointer to head
+ * Return: list length
+ */
+unsigned int listy(dlistint_t *h)
+{
+	unsigned int nc = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		nc++;
+	}
+	return (nc);
 }
