ec596f51ea6084dfd039cd83aa44b923bf51c0d2
Corbin Enterline <corbinenterline1@gmail.com>
2020-05-29 20:24:16 +0000

    varread

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,7 +3,26 @@
 /**
  * hash_table_create - Creates a new Hash Table!
  * @size: Size of the associative array
- *
- *
+ * Return: Pointer to new hash table, or NULL if something fails
  */
-hash_table_t *hash_table_create(unsigned long int size);
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *nt = malloc(sizeof(hash_table_t));/** (n)ew (t)able */
+	unsigned long int c = 0;/** loop (c)ounter */
+
+	if (nt == NULL)/** malloc check */
+		return (NULL);
+	nt->size = size;/** assign size value */
+	if (size != 0)
+	{
+		nt->array = malloc((sizeof(hash_table_t)) * size);
+		if (nt->array == NULL)
+		{/** malloc check, free table if array failed */
+			free(nt);
+			return (NULL);
+		}
+	}
+	for (; c < size; c++)/** initialize indexes of array to NULL */
+		nt->array[c] = NULL;
+	return (nt);
+}
