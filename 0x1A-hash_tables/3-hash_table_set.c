799c662f125921d2670a38eef0c9c2d154acb19a
Corbin Enterline <corbinenterline1@gmail.com>
2020-05-29 22:57:48 +0000

    varread

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,32 +8,36 @@
  * Return: 1 if succeeded, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
-{
+{/* node is new node, sniffy is for searching(sniffing) stuff */
 	hash_node_t *node = NULL, *sniffy = NULL;
 	unsigned long int ki;/* (k)ey (i)ndex result */
 
 	if (key == NULL || ht == NULL)/* no empty table or key */
 		return (0);
-	node = malloc(sizeof(hash_node_t));/* malloc node */
-	if (node == NULL)/* malloc check */
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
 		return (0);
 	node->key = strdup(key);
-	if (node->key == NULL)
-	{
-		free(node);
-		return (0);
-	}
 	node->value = strdup(value);
-	if (node->value == NULL)
+	ki = key_index((const unsigned char *)key, ht->size);/* key index */
+	sniffy = ht->array[ki];/* set sniffy to key index of array */
+	while (sniffy)/* if list exists, check if key exists. Update if so */
 	{
-		free(node->key);
-		free(node);
-		return (0);
+		if (strcmp(sniffy->key, key) == 0)
+		{
+			free(sniffy->value);
+			sniffy->value = strdup(value);
+			return (1);
+		}
+		sniffy = sniffy->next;
 	}
-	ki = key_index((const unsigned char *)key, ht->size);
 	sniffy = ht->array[ki];
 	if (sniffy)
+	{
 		node->next = sniffy;
+		ht->array[ki] = node;
+
+	}
 	else
 	{
 		node->next = NULL;
