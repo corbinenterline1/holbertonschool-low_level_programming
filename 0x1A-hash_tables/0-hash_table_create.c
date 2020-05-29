ca69b91bbdc3bcbc5bcd8b35581d200c8783a77a
Corbin Enterline <corbinenterline1@gmail.com>
2020-05-29 19:57:47 +0000

    Uploading for credential store

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,9 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a new Hash Table!
+ * @size: Size of the associative array
+ *
+ *
+ */
+hash_table_t *hash_table_create(unsigned long int size);
